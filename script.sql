24cb28a44d65cc22c37dbb29550f8b4481873f7a
Wesslleey <147674333+Wesslleey@users.noreply.github.com>
2023-10-12 01:47:53 +0000

    aula do dia 11/10/23

---
+++ script.sql
@@ -0,0 +1,9 @@
+-- Exibir os bancos de dados do servidor
+# Comentário (MYSQL)
+SHOW DATABASES;
+
+-- Exibir tabelas de banco de dados específicos
+SHOW TABLES FROM information_schema
+
+-- Criação de nova base de dados
+CREATE DATABASE banco_de_teste;
