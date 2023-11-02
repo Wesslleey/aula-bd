a7a60eee9fdb3457954a22bec2bea76b8dc9269b
Wesslleey <147674333+Wesslleey@users.noreply.github.com>
2023-11-02 01:45:31 +0000

    aula01/11

--- script.sql
+++ script.sql
@@ -7,3 +7,25 @@ SHOW TABLES FROM information_schema
 
 -- Criação de nova base de dados
 CREATE DATABASE banco_de_teste;
+
+-- Remover banco de dados
+
+DROP DATABASE;
+
+CREATE TABLE banco_de_teste.alunos(
+    id INT PRIMARY KEY,
+    nome VARCHAR (255)
+);
+
+SHOW TABLES FROM banco_de_teste;
+
+SHOW FIELDS FROM banco_de_teste.alunos;
+
+USE banco_de_teste;
+
+CREATE TABLE teste(
+    id INT PRIMARY KEY,
+    valor CHAR(11)
+)
+
+
